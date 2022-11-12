1d40a80182ad377906fa155f4aab3d76cdcdbf13
Matias Guarnera <matiasguarnera@gmail.com>
2022-11-12 18:07:32 -0300

    Regular and Interpolated

--- Dev Tools Dom/main.js
+++ Dev Tools Dom/main.js
@@ -2,14 +2,14 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
 function makeGreen() {
   const p = document.querySelector('p');
-  p.style.color = '#BADA55';
-  p.style.fontSize = '50px';
+  p.style.color = '#B0CAAA';
+  p.style.fontSize = '100px';
 }
 
 // Regular
-
+console.log('hello')
 // Interpolated
-
+console.log('Hello I am a %s string!', 'BOCAAAAA')
 // Styled
 
 // warning!
