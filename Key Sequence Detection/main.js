b065b8aa0387a1c790163ae422ea6c8a06871a50
Matias Guarnera <matiasguarnera@gmail.com>
2022-11-14 10:11:27 -0300

    KSD Finished with cornify to add unicorns and rainbows. I can do something similar for my portfolio

--- Key Sequence Detection/main.js
+++ Key Sequence Detection/main.js
@@ -6,6 +6,11 @@ window.addEventListener('keyup', (event) => {
     console.log(event.key)
     press.push(event.key)
     press.splice(-code.length-1, press.length-code.length)
+
+    if(press.join("").includes(code)){
+        console.log("SECRET CODE UNLOCKED")
+        cornify_add()
+    }
     console.log(press)
 })
 
