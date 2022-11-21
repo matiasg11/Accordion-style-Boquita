88615aac016e50fcaf2360eb871a4688618d5723
Matias Guarnera <matiasguarnera@gmail.com>
2022-11-21 14:10:45 -0300

    Good understanding of how the images add or not the class 'active'

--- Slide in on scroll/main.js
+++ Slide in on scroll/main.js
@@ -1,5 +1,5 @@
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
+function debounce(func, wait = 10, immediate = true) {
+    var timeout; //It runs the function AT MOST every 10ms
     return function() {
       var context = this, args = arguments;
       var later = function() {
@@ -12,3 +12,29 @@ function debounce(func, wait = 20, immediate = true) {
       if (callNow) func.apply(context, args);
     };
   }
+
+
+const imagesSliding = document.querySelectorAll(".slide-in")
+
+function checkSlide(event){
+    imagesSliding.forEach(image => {
+        //Halfway through the image it should appear.
+        const slideInAt = window.scrollY +  window.innerHeight - image.height/2
+        console.log(slideInAt) 
+        //Once it passed it should be back out.
+        const imgBottom = image.offsetTop + image.height
+
+        const isHalfShown = slideInAt > image.offsetTop;
+        const isNotScrolledPast = window.scrollY < imgBottom
+        if (isHalfShown && isNotScrolledPast){
+            image.classList.add('active')
+        }else{
+            image.classList.remove('active')
+        }
+    })
+    console.count(event)
+}
+
+window.addEventListener('scroll', debounce(checkSlide))
+
+//Loop all the images and when it is peaked, show them
