ad87186618f3395ab77780c967913e778d5c4b44
Matias Guarnera <matiasguarnera@gmail.com>
2022-11-23 17:50:23 -0300

    Done: the trick is in using the strip function in the comparison but not on the elements of the array

---
+++ Sort w-o articles/main.js
@@ -0,0 +1,19 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+
+function strip(band){
+    return band.replace(/^(a |the |an )/i, "".trim())    
+}
+
+let sortedBands = bands.sort(function (a,b){
+    if(strip(a)>strip(b)){  
+    //Only using the strip function in the comparison 
+        return 1
+    }else{
+        return -1
+    }
+})
+
+document.querySelector('#bands').innerHTML = sortedBands.map(band =>`<li>${band}</li>`)
+
+console.log(sortedBands)
