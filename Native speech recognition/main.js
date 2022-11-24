c6b6464c55c859a70d53c34e66bb1037d3ac8aeb
Matias Guarnera <matiasguarnera@gmail.com>
2022-11-24 17:38:10 -0300

    Speech recognition in progress

--- Native speech recognition/main.js
+++ Native speech recognition/main.js
@@ -1,1 +1,16 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+//It is a global variable that lives on the browser
+
+const recognition = new SpeechRecognition();
+recongnition.interimResults = true; //to put the results in the middle of hearing it.
+
+//it updates the paragraph and then creates a new one when stops hearing.
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+
+recongnition.addEventListener('results', e => {
+    console.log(e)
+})
