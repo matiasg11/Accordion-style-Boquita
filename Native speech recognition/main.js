f4fe3f1d499beae7000a3db62d346bd8af7c70d5
Matias Guarnera <matiasguarnera@gmail.com>
2022-11-24 18:43:36 -0300

    Speech recognition and writing done. When it ends, it starts again

--- Native speech recognition/main.js
+++ Native speech recognition/main.js
@@ -12,12 +12,24 @@ words.appendChild(p);
 
 
 recognition.addEventListener('result', e => {
-    console.log(e.results) 
+    //console.log(e.results) 
     /*It has SpeechRecognitionResult and inside it, 
     SpeechRecognitionAlternative, which has the transcript 
     and the confidence. It also has the isFinal variable, 
     to determine if the person stopped talking.*/
     let transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+
+    p.textContent = transcript;
+    if(e.results[0].isFinal){
+        p = document.createElement('p');
+        words.appendChild(p)
+    }
+
+    console.log(transcript)
 })
 
+recognition.addEventListener('end', recognition.start)
 recognition.start()
