02f215774af244cf02ce4dbd916c8bc600e13964
Matias Guarnera <matiasguarnera@gmail.com>
2022-11-26 19:36:45 -0300

    Function Add Item

--- Local Storage and Event Delegation/main.js
+++ Local Storage and Event Delegation/main.js
@@ -1,4 +1,15 @@
 
-  const addItems = document.querySelector('.add-items');
-  const itemsList = document.querySelector('.plates');
-  const items = [];
+  const addItems = document.querySelector('.add-items'); //Form Element
+  const itemsList = document.querySelector('.plates'); //List of plates
+  const items = []; //Storing all the data in an array of objects
+
+  addItems.addEventListener('submit', addItem)
+
+  function addItem(e){
+    e.preventDefault(); //Prevents the refreshing of the page. 
+    const text = this.querySelector('[name=item]') //This is the form element and selects the field with the name "item"
+    const item = {
+        text: 'item name',
+        done: false,
+    }
+  }
