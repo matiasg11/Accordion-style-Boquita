19ada047b18cdbacd3d7c1b650e808c1fbf31f9d
Matias Guarnera <matiasguarnera@gmail.com>
2022-12-10 20:21:37 -0300

    Selection of classes for better handling

---
+++ Stripe follow Along/main.js
@@ -0,0 +1,3 @@
+const triggers = document.querySelectorAll(".cool > li") //All the li which are direct sons of .cool (the ul)
+const background = document.querySelector('.dropdownBackground') //The background, which has this class
+const nav = document.querySelector('.top') //the nav, which has this class.
